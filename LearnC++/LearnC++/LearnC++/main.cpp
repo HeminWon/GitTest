ba2b960043b381bb34428e5625a36d1406971b95
HeminWon <waiting4254@gmail.com>
2019-03-29 19:59:32 +0800

    c++

---
+++ LearnC++/LearnC++/LearnC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LearnC++
+//
+//  Created by 王明海 on 2019/3/29.
+//  Copyright © 2019 HeminWon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
