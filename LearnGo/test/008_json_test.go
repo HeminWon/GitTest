fb82c4e7352191862cf2def8e81ee3c0c6191d6d
Hemin Won <waiting4254@gmail.com>
2019-06-10 15:23:23 +0800

    sort

--- LearnGo/test/008_json_test.go
+++ LearnGo/test/008_json_test.go
@@ -6,23 +6,23 @@ import (
 )
 
 type Movie struct {
-    Title  string
-    Year   int  `json:"released"`
-    Color  bool `json:"color,omitempty"`
-    Actors []string
+	Title  string
+	Year   int  `json:"released"`
+	Color  bool `json:"color,omitempty"`
+	Actors []string
 }
 
 var movies = []Movie{
-    {Title: "Casablanca", Year: 1942, Color: false,
-        Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
-    {Title: "Cool Hand Luke", Year: 1967, Color: true,
-        Actors: []string{"Paul Newman"}},
-    {Title: "Bullitt", Year: 1968, Color: true,
-        Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
-    // ...
+	{Title: "Casablanca", Year: 1942, Color: false,
+		Actors: []string{"Humphrey Bogart", "Ingrid Bergman"}},
+	{Title: "Cool Hand Luke", Year: 1967, Color: true,
+		Actors: []string{"Paul Newman"}},
+	{Title: "Bullitt", Year: 1968, Color: true,
+		Actors: []string{"Steve McQueen", "Jacqueline Bisset"}},
+	// ...
 }
 
-func TestJsonMarshal (t *testing.T) {
+func TestJsonMarshal(t *testing.T) {
 
 	// if data, err := json.Marshal(movies); err != nil {
 	// 	t.Log(err)
@@ -30,10 +30,10 @@ func TestJsonMarshal (t *testing.T) {
 	// 	t.Logf("%s\n",data)
 	// }
 	if data, err := json.Marshal(movies); err == nil {
-		t.Logf("%s\n",data)
+		t.Logf("%s\n", data)
 	} else {
 		t.Log(err)
 		return
 	}
-	
+
 }
---
+++ LearnGo/test/009_struct_sort_test.go
@@ -0,0 +1,73 @@
+package test
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+type Persons []Person
+
+// 获取此 slice 的长度
+func (p Persons) Len() int { return len(p) }
+
+// 根据元素的年龄降序排序 （此处按照自己的业务逻辑写）
+func (p Persons) Less(i, j int) bool {
+	return strings.ToLower(p[i].Name) < strings.ToLower(p[j].Name)
+}
+
+// 交换数据
+func (p Persons) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+
+func TestStructSort(t *testing.T) {
+	persons := Persons{
+		{
+			Name: "xchuxia",
+			Age:  20,
+		},
+		{
+			Name: "dongtian",
+			Age:  22,
+		},
+		{
+			Name: "ftgyhujim",
+			Age:  21,
+		},
+		{
+			Name: "tuya example widget",
+			Age:  20,
+		},
+		{
+			Name: "Tuya OEM com xxx test aaa watchkitapp",
+			Age:  22,
+		},
+		{
+			Name: "tuya OEM com xxx test aaa watchkitapp2",
+			Age:  22,
+		},
+		{
+			Name: "Tuya OEM com xxx test aaa watchkitapp1",
+			Age:  22,
+		},
+		{
+			Name: "xxxxxxx",
+			Age:  21,
+		},
+	}
+
+	fmt.Println("排序前")
+	for _, person := range persons {
+		fmt.Println(person.Name, ":", person.Age)
+	}
+	sort.Sort(persons)
+	fmt.Println("排序后")
+	for _, person := range persons {
+		fmt.Println(person.Name, ":", person.Age)
+	}
+}
