70c0082f6fe65171af40b0ff9bf13cfe1abd95c2
Hemin Won <waiting4254@gmail.com>
2019-06-22 23:52:55 +0800

    add sync

---
+++ LearnGo/test/017_share_mem_test.go
@@ -0,0 +1,34 @@
+package test
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCounter(t *testing.T) {
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			counter++
+		}()
+	}
+	time.Sleep(time.Second * 1)
+	t.Logf("counter = %d", counter)
+}
+
+func TestCounterThreadSafe(t *testing.T) {
+	mut := &sync.Mutex{}
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	time.Sleep(time.Second * 1)
+	t.Logf("counter = %d", counter)
+}
